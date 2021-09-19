24c71f110919451bac0524c80929f1454fabf857
pmishra7683 <90520662+pmishra7683@users.noreply.github.com>
2021-09-19 12:40:55 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,48 @@
+function validate() {
+var text;
+   if( document.myForm.name.value == "" ) {
+     text = "Name cannot be empty";
+      document.getElementById("demo").innerHTML = text;
+      document.myForm.name.focus() ;
+      return false;
+   }
+   if( document.myForm.email.value == "" ) {
+     text = "E-mail cannot be empty";
+      document.getElementById("demo").innerHTML = text;
+      document.myForm.email.focus() ;
+      return false;
+   }
+var emailID = document.myForm.email.value;
+atposn = emailID.indexOf("@");
+dotposn = emailID.lastIndexOf(".");
+if (atposn < 1 || ( dotposn - atposn < 2 )) {
+text = "Please enter valid email ID";
+document.getElementById("demo").innerHTML = text;
+document.myForm.email.focus() ;
+return false;
+}
+   if( document.myForm.monumber.value == "" || isNaN( document.myForm.phone.value ) ||
+      document.myForm.monumber.value.length != 10 ) {
+      text = "Please enter a valid 10-digit phone number";
+      document.getElementById("demo").innerHTML = text;
+      document.myForm.monumber.focus() ;
+      return false;
+   }
+   if( document.myForm.Password.value == "" || isNaN( document.myForm.Password.value ) ||
+      document.myForm.monumber.value.length != 10 ) {
+      text = "Please enter a valid 10-digit phone number";
+      document.getElementById("demo").innerHTML = text;
+      document.myForm.monumber.focus() ;
+      return false;
+   }
+//    if( document.myForm.subject.value == "0" ) {
+//       text = "Please provide your area of expertise";
+//       document.getElementById("demo").innerHTML = text;
+//       return false;
+//    }
+   return( true );
+}
+
+
+
+
